de8a9259e470fbf93850d88fd4e21da65d33b20f
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-11 22:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,9 +17,17 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  let count = 0;
+	for (let index = 0; index < library.length; index++) {
+		let temp = library[i];
+	if(temp.readingStatus)
+	{
+		count++;
+	}
+	}
+	return count;
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+// alert(numberOfBooksRead());
